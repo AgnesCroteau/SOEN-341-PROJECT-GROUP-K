e35c9a9aa2ce63f6fd242e3a11321dceff4d351e
Nolan Bastien <n_astien@live.concordia.ca>
2022-02-23 11:52:01 -0500

    Have on hover to change style of titles

--- src/Components/ItemCard.js
+++ src/Components/ItemCard.js
@@ -1,28 +1,43 @@
+import { useState } from "react";
 import { Button, Card, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
-
 function ItemCard(props) {
-  
+  const [isHovering, setHover] = useState(false);
+
   let navigate = useNavigate();
+
+  const toggleHover = () => {
+    setHover(!isHovering);
+  };
 
   return (
     <Container>
-      <Card style={{ width: "18rem", margin: "10%" }}>
+      <Card
+        style={{ width: "18rem", margin: "10%" }}
+        onDoubleClick={() => {
+          navigate("/item" + props.id);
+        }}
+      >
         <Card.Img variant="top" src={props.img} />
         <Card.Body>
           <Card.Title
             onClick={() => {
-              navigate(("/item" + props.id));
+              navigate("/item" + props.id);
             }}
+            onMouseEnter={toggleHover}
+            onMouseLeave={toggleHover}
+            style={{ color: isHovering ? "red" : "" }}
           >
             <b>{props.title}</b>
           </Card.Title>
-          <Card.Text>
-            {props.description}
-          </Card.Text>
+          <Card.Text>{props.description}</Card.Text>
           <Card.Footer>Price : ${props.price}</Card.Footer>
-          <Button class="addToCartBtn" variant="primary">
+          <Button
+            class="addToCartBtn"
+            variant="primary"
+            style={{ marginTop: "10px" }}
+          >
             Add to cart
           </Button>
         </Card.Body>
