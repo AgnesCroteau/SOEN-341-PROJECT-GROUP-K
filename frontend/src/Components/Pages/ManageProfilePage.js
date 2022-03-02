2cb929d68d0f3ea58ec76d1b47787665531f1526
KellyA <kelly.acoca@gmail.com>
2022-03-02 16:19:20 -0500

    New Managing Profile Page

--- frontend/src/Components/Pages/ManageProfilePage.js
+++ frontend/src/Components/Pages/ManageProfilePage.js
@@ -1,4 +1,4 @@
-import { Button, Card, Container, Form } from 'react-bootstrap';
+import { Button, Card, Container, Form, Stack } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 
 
@@ -7,34 +7,57 @@ function ManageProfilePage() {
     let navigate = useNavigate();
 
     return (
-        <Container fluid style={{display: 'flex', justifyContent: 'center' }}>
-            
-            
-            <h1 onClick={() => {navigate('/')}} style={{textAlign: "center", marginTop: '3%', fontWeight:"bold"}}>BOREAL.ca</h1>
-            
-            <Card style={{ width: '18rem', position: 'absolute', top: '15%', outline: "solid" }}>
-            <h3 style={{ marginTop: '5%', textAlign: "center" }}>Edit Account Information</h3>
-                <Card.Body>
-                    
+        <Container fluid style={{ display: 'flex', justifyContent: 'center' }}>
+
+
+            <h1 onClick={() => { navigate('/') }} style={{ textAlign: "center", marginTop: '2%', fontWeight: "bold" }}>BOREAL.ca</h1>
+
+            <Card style={{ width: '35rem', position: 'absolute', top: '12%', outline: "solid" }}>
+                <h3 style={{ textAlign: "center" }}>Edit Account Information</h3>
+                <Card.Body> <Stack direction="horizontal" gap={3}>
+                    <div className="ms-auto"><Card.Link href="#">Edit</Card.Link></div>
+                </Stack>
+
                     <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>New Name</Form.Label>
-                            <Form.Control type="name" placeholder="Enter Your New Name" />
+                        <Form.Group className="mb-2" controlId="formBasicName">
+                            <Form.Label>Full Name</Form.Label>
+
+                            <Form.Control type="name" placeholder="Kelly Acoca" />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>New Email</Form.Label>
-                            <Form.Control type="name" placeholder="Enter Your New Email" />
+                        <Form.Group className="mb-2" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="name" placeholder="Kelly.acoca@gmail.com" />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>New Password</Form.Label>
+
+                        <Form.Group className="mb-2" controlId="formBasicPassword">
+                            <Form.Label>Your Password</Form.Label>
                             <Form.Control type="name" placeholder="Enter Your New Password" />
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Group className="mb-2" controlId="formBasicAddress">
+                            <Form.Label>Your Home Address</Form.Label>
+                            <Form.Control type="name" placeholder="5825 Ave Eldridge, H4W2E3, Cote-Saint-Luc, Quebec, Canada" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-2" controlId="formBasicNumber">
+                            <Form.Label>Your Phone Number</Form.Label>
+                            <Form.Control type="name" placeholder="514-550-8326" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-2" controlId="formBasicGender">
+                            <Form.Label>Gender</Form.Label>
+
+                            <Form.Select>
+                                <option>Female</option>
+                                <option>Male</option>
+                            </Form.Select>
+                        </Form.Group>
+
+                        <Form.Group className="mb-2" controlId="formBasicAccount">
                             <Form.Label>Account Type</Form.Label>
-                            
+
                             <Form.Select>
                                 <option>Customer</option>
                                 <option>Seller</option>
@@ -42,12 +65,14 @@ function ManageProfilePage() {
                             </Form.Select>
                         </Form.Group>
                        
+                        <Form.Group className="mb-4" controlId="formBasicChange">
+                        </Form.Group>
+
                         <div className='d-grid'>
                             <Button variant="primary" type="submit">
-                                Edit Information
+                                Save Changes
                             </Button>   
                         </div>
-                        
                     </Form>
                 </Card.Body>
             </Card>
