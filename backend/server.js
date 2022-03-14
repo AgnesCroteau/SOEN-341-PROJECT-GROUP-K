1c2539a0b90e2825af4c4d5c90a9bd858a00a274
cnfrancis <37379852+cnfrancis@users.noreply.github.com>
2022-03-14 11:59:16 -0400

    tested api endpoints and handle http response

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://dev:hCoYCjUTGFKEC2IK@boreal.vaa1q.mongodb.net/boreal_db?retryWrites=true&w=majority";
 
 const express = require('express');
+const app = express();
 const { response } = require('express');
 
 app.use(express.json());
@@ -31,11 +32,13 @@ async function writeNewUserProfileToDb(uri_, user_info) {
     });
     const db = client.db("boreal_db");
     var users_tb = db.collection("user_accounts_info");
-    await users_tb.insertOne(user_info);
+    const response = await users_tb.insertOne(user_info);
     client.close();
+    return response;
 
   } catch (error) {
     console.log(error);
+    client.close();
   }
   
 }
@@ -47,7 +50,7 @@ async function updateUserProfileToDb(uri_, user_info) {
     });
     const db = client.db("boreal_db");
     var users_tb = db.collection("user_accounts_info");
-    await users_tb.updateOne({"_id": user_info._id},{
+    const response = await users_tb.updateOne({"_id": user_info._id},{
       $set: {
         "full_name": user_info.full_name,
         "email": user_info.email,
@@ -59,7 +62,9 @@ async function updateUserProfileToDb(uri_, user_info) {
       }
     })
     client.close();
+    return response;
   } catch(error) {
+    client.close();
     console.log(error);
   }
 }
@@ -71,17 +76,17 @@ app.post('/storeUserAccountInfo', function(req, res) {
   res.set({
     'Access-Control-Allow-Origin': '*'
   })
-  writeNewUserProfileToDb(uri, req.body).then(response => console.log(response));
+  writeNewUserProfileToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
 });
 
 app.post('/updateUserAccountInfo', function(req, res) {
   res.set({
     'Access-Control-Allow-Origin': '*'
   });
-  updateUserProfileToDb(uri, req.body).then(response => console.log);
+  updateUserProfileToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
 });
 
-getProductsFromDatabase(uri).then(response => { products = response; });
+getProductsFromDatabase(uri).then(response => { products = response;});
 
 
 async function writeOrderInDB(order) {
