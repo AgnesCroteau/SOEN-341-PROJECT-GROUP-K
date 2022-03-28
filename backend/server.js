4ed2caa44baec5589134bb74090e937e6346fa3d
emrb9 <93956512+emrb9@users.noreply.github.com>
2022-03-27 21:01:29 -0400

    added comments

--- backend/server.js
+++ backend/server.js
@@ -15,35 +15,12 @@ process.on("uncaughtException", function (err) {
   console.log(err);
 });
 
-// Functions to access database
-async function getProductsFromDatabase() {
-  const client = await MongoClient.connect(uri, {
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-  });
-  const db = client.db("boreal_db");
-  var products_tb = db.collection("products");
-  let allproducts = await products_tb.find().toArray();
+/*
+PROFILE (ALL)
+*/
 
-  client.close();
-  return allproducts;
-}
-async function retrieveOrderInDb(uri_, user_info) {
-  try {
-  const client = await  MongoClient.connect(uri_, {
-    useUnifiedTopology: true, serverApi: ServerApiVersion.v1
-  });
-  const db = client.db("boreal_db");
-  var orders_tb = db.collection("orders");
-  const response = await orders_tb.find({"customer_id": user_info.customer_id},{
-  }).toArray();
-  client.close();
-  return response;
-} catch(error) {
-  client.close();
-  console.log(error);
-}
-}
+//SIGN UP, INPUT: full_name, email, password, account_Type (_id IS UNIQUE, USER )
+//SIGN UP, OUTPUT: new user registered in the database
 async function writeNewUserProfileToDb(uri_, user_info) {
   try {
     const client = await  MongoClient.connect(uri_, {
@@ -54,14 +31,14 @@ async function writeNewUserProfileToDb(uri_, user_info) {
     const response = await users_tb.insertOne(user_info);
     client.close();
     return response;
-
   } catch (error) {
     console.log(error);
     client.close();
   }
-  
 }
 
+//EDIT PROFILE, INPUT: full_name, email, password, account_Type,(_id CANNOT BE MODIFIED)
+//EDIT PROFILE, OUTPUT: updates user info in database
 async function updateUserProfileToDb(uri_, user_info) {
   try {
     const client = await  MongoClient.connect(uri_, {
@@ -69,6 +46,7 @@ async function updateUserProfileToDb(uri_, user_info) {
     });
     const db = client.db("boreal_db");
     var users_tb = db.collection("user_accounts_info");
+    //This will retrieve the _id associated to the logged in user
     const response = await users_tb.updateOne({"_id": user_info._id},{
       $set: {
         "full_name": user_info.full_name,
@@ -88,6 +66,8 @@ async function updateUserProfileToDb(uri_, user_info) {
   }
 }
 
+//AUTHENTICATION, INPUT: email, password
+//AUTHENTICATION, OUTPUT: logged in user (valid credential) OR error: wrong email or password (invalid credentials) 
 async function validateUserProfileToDb(uri_, user_info) {
   try {
   const client = await  MongoClient.connect(uri_, {
@@ -105,98 +85,7 @@ async function validateUserProfileToDb(uri_, user_info) {
 }
 }
 
-//Create an API endpoint to add a product to the database (_id can be viewed as SKU; unique)
-async function writeNewSellerProductToDb(uri_, seller_info) {
-  try {
-    const client = await  MongoClient.connect(uri_, {
-      useUnifiedTopology: true, serverApi: ServerApiVersion.v1
-    });
-    const db = client.db("boreal_db");
-    var products_tb = db.collection("products");
-    const response = await products_tb.insertOne(seller_info);
-    client.close();
-    return response;
-
-  } catch (error) {
-    console.log(error);
-    client.close();
-  }
-  
-}
-//Create an API endpoint to modify a product to the database
-async function updateSellerProductToDb(uri_, seller_info) {
-  try {
-    const client = await  MongoClient.connect(uri_, {
-      useUnifiedTopology: true, serverApi: ServerApiVersion.v1
-    });
-    const db = client.db("boreal_db");
-    var products_tb = db.collection("products");
-    const response = await products_tb.updateOne({"_id": seller_info._id},{
-      $set: {
-        //Seller is able to modify title, description, price and img ONLY. 
-        "title": seller_info.title,
-        "description": seller_info.description,
-        "price": seller_info.price,
-        "img": seller_info.img,
-      }
-    })
-    client.close();
-    return response;
-  } catch(error) {
-    client.close();
-    console.log(error);
-  }
-}
-//Create API endpoint to see all items I have posted as a seller (input: seller_id; output: seller's products)
-async function retrieveProductsInDb(uri_, seller_info) {
-  try {
-  const client = await  MongoClient.connect(uri_, {
-    useUnifiedTopology: true, serverApi: ServerApiVersion.v1
-  });
-  const db = client.db("boreal_db");
-  var products_tb = db.collection("products");
-  const response = await products_tb.find({"seller_id": seller_info.seller_id},{
-  }).toArray();
-  client.close();
-  return response;
-} catch(error) {
-  client.close();
-  console.log(error);
-}
-}
-//Delete seller product using _id (SKU) of product
-async function deleteProductInDb(uri_, seller_info) {
-  try {
-    const client = await  MongoClient.connect(uri_, {
-      useUnifiedTopology: true, serverApi: ServerApiVersion.v1
-    });
-    const db = client.db("boreal_db");
-    var products_tb = db.collection("products");
-    const response = await products_tb.deleteOne({"_id": seller_info._id},{
-    })
-    client.close();
-    return response;
-  } catch(error) {
-    client.close();
-    console.log(error);
-  }
-}
-
-app.post('/updateProduct', function(req, res) {
-  res.set({
-    'Access-Control-Allow-Origin': '*'
-  });
-  updateSellerProductToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
-});
-
-app.delete('/deleteProduct', function(req, res) {
-  console.log(req.body);
-  res.set({
-    'Access-Control-Allow-Origin': '*'
-  })
-  deleteProductInDb(uri, req.body).then(response => {console.log(response); res.send(response)});
-});
-
+//SIGN UP 
 app.post('/storeUserAccountInfo', function(req, res) {
   console.log(req.body);
   res.set({
@@ -205,6 +94,7 @@ app.post('/storeUserAccountInfo', function(req, res) {
   writeNewUserProfileToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
 });
 
+//EDIT PROFILE
 app.post('/updateUserAccountInfo', function(req, res) {
   res.set({
     'Access-Control-Allow-Origin': '*'
@@ -212,36 +102,23 @@ app.post('/updateUserAccountInfo', function(req, res) {
   updateUserProfileToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
 });
 
+//AUTHENTICATION
 app.post('/verifyUserAccountInfo', function(req, res) {
   res.set({
     'Access-Control-Allow-Origin': '*'
   })
   validateUserProfileToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
 });
-//Create API endpoint to see all items I have posted as a seller
-app.get("/retrieveProducts", (req, res) => {
-  res.set({ "Access-Control-Allow-Origin": "*" });
 
-  retrieveProductsInDb(uri, req.body).then((response) => {
-    res.send(response);
-  });
-});
 
-app.post('/addProduct', function(req, res) {
-  console.log(req.body);
-  res.set({
-    'Access-Control-Allow-Origin': '*'
-  })
-  writeNewSellerProductToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
-});
 
-app.get("/retrieveOrder", (req, res) => {
-  res.set({ "Access-Control-Allow-Origin": "*" });
 
-  retrieveOrderInDb(uri, req.body).then((response) => {
-    res.send(response);
-  });
-});
+/*
+ORDERS (USER)
+*/
+
+//ADD ORDER, INPUT: customer_id, products_id:[Array], (_id IS UNIQUE, ORDER NUMBER)
+//ADD ORDER, OUTPUT: order is added to database
 async function writeOrderInDB(order) {
   if (order === undefined) return "Error. Order is not defined.";
   const client = await MongoClient.connect(uri, {
@@ -261,7 +138,192 @@ async function writeOrderInDB(order) {
   return requestStatus;
 }
 
-// API endpoints as functions
+//RETRIEVE MY ORDERS, INPUT: customer_id
+//RETRIEVE MY ORDERS, OUTPUT: returns all orders associated to customer_id 
+async function retrieveOrderInDb(uri_, user_info) {
+  try {
+  const client = await  MongoClient.connect(uri_, {
+    useUnifiedTopology: true, serverApi: ServerApiVersion.v1
+  });
+  const db = client.db("boreal_db");
+  var orders_tb = db.collection("orders");
+  //retrieve all orders associated to customer_id
+  const response = await orders_tb.find({"customer_id": user_info.customer_id},{
+  }).toArray();
+  client.close();
+  return response;
+} catch(error) {
+  client.close();
+  console.log(error);
+}
+}
+
+//ADD ORDER
+app.post("/sendOrder", (req, res) => {
+  console.log("\n[API endpoint - send Order]\n\nOrder to add: ");
+  console.log(req.body);
+  res.set({ "Access-Control-Allow-Origin": "*" });
+  writeOrderInDB(req.body).then((response) => {
+    console.log("\nResponse is:");
+    console.log(response);
+    console.log();
+    res.send(response);
+  });
+});
+
+//RETRIEVE MY ORDERS
+app.get("/retrieveOrder", (req, res) => {
+  res.set({ "Access-Control-Allow-Origin": "*" });
+  retrieveOrderInDb(uri, req.body).then((response) => {
+    res.send(response);
+  });
+});
+
+
+
+
+/*
+PRODUCTS (SELLER)
+*/
+
+//ADD PRODUCT, INPUT: _id, title, description, price, img, seller_id (_id IS UNIQUE; SKU)
+//ADD PRODUCT, OUTPUT: product is added to database
+async function writeNewSellerProductToDb(uri_, seller_info) {
+  try {
+    const client = await  MongoClient.connect(uri_, {
+      useUnifiedTopology: true, serverApi: ServerApiVersion.v1
+    });
+    const db = client.db("boreal_db");
+    var products_tb = db.collection("products");
+    const response = await products_tb.insertOne(seller_info);
+    client.close();
+    return response;
+  } catch (error) {
+    console.log(error);
+    client.close();
+  }
+}
+
+//EDIT PRODUCT, INPUT: title, description, price, image, (seller_id CANNOT BE MODIFIED)
+//EDIT PRODUCT, OUTPUT: updates product info in database
+async function updateSellerProductToDb(uri_, seller_info) {
+  try {
+    const client = await  MongoClient.connect(uri_, {
+      useUnifiedTopology: true, serverApi: ServerApiVersion.v1
+    });
+    const db = client.db("boreal_db");
+    var products_tb = db.collection("products");
+    //This will retrieve the _id(SKU) associated to the product
+    const response = await products_tb.updateOne({"_id": seller_info._id},{
+      $set: {
+        //Seller is able to modify title, description, price and img ONLY. 
+        "title": seller_info.title,
+        "description": seller_info.description,
+        "price": seller_info.price,
+        "img": seller_info.img,
+      }
+    })
+    client.close();
+    return response;
+  } catch(error) {
+    client.close();
+    console.log(error);
+  }
+}
+
+//DELETE PRODUCT, INPUT: _id (SKU) of product
+//DELETE PRODUCT, OUTPUT:  remove product associated to _id(SKU) from database
+async function deleteProductInDb(uri_, seller_info) {
+  try {
+    const client = await  MongoClient.connect(uri_, {
+      useUnifiedTopology: true, serverApi: ServerApiVersion.v1
+    });
+    const db = client.db("boreal_db");
+    var products_tb = db.collection("products");
+    const response = await products_tb.deleteOne({"_id": seller_info._id},{
+    })
+    client.close();
+    return response;
+  } catch(error) {
+    client.close();
+    console.log(error);
+  }
+}
+
+//RETRIEVE MY PRODUCTS, INPUT: seller_id
+//RETRIEVE MY PRODUCTS, OUTPUT: returns all products associated to seller_id 
+async function retrieveProductsInDb(uri_, seller_info) {
+  try {
+  const client = await  MongoClient.connect(uri_, {
+    useUnifiedTopology: true, serverApi: ServerApiVersion.v1
+  });
+  const db = client.db("boreal_db");
+  var products_tb = db.collection("products");
+  //retrieve all products associated to seller_id 
+  const response = await products_tb.find({"seller_id": seller_info.seller_id},{
+  }).toArray();
+  client.close();
+  return response;
+} catch(error) {
+  client.close();
+  console.log(error);
+}
+}
+
+//ADD PRODUCT
+app.post('/addProduct', function(req, res) {
+  console.log(req.body);
+  res.set({
+    'Access-Control-Allow-Origin': '*'
+  })
+  writeNewSellerProductToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
+});
+
+//EDIT PRODUCT 
+app.post('/updateProduct', function(req, res) {
+  res.set({
+    'Access-Control-Allow-Origin': '*'
+  });
+  updateSellerProductToDb(uri, req.body).then(response => {console.log(response); res.send(response)});
+});
+
+//DELETE PRODUCT
+app.delete('/deleteProduct', function(req, res) {
+  console.log(req.body);
+  res.set({
+    'Access-Control-Allow-Origin': '*'
+  })
+  deleteProductInDb(uri, req.body).then(response => {console.log(response); res.send(response)});
+});
+
+//RETRIEVE MY PRODUCTS
+app.get("/retrieveProducts", (req, res) => {
+  res.set({ "Access-Control-Allow-Origin": "*" });
+  retrieveProductsInDb(uri, req.body).then((response) => {
+    res.send(response);
+  });
+});
+
+/*
+BROWSE ITEMS (CUSTOMER)
+*/
+
+//BROWSE ITEMS, INPUT: GET REQUEST
+//BROWSE ITEMS, OUTPUT: returns all products in the database 
+async function getProductsFromDatabase() {
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  const db = client.db("boreal_db");
+  var products_tb = db.collection("products");
+  let allproducts = await products_tb.find().toArray();
+
+  client.close();
+  return allproducts;
+}
+
+//BROWSE ITEMS 
 app.get("/getAllProducts", (req, res) => {
   res.set({ "Access-Control-Allow-Origin": "*" });
 
@@ -270,19 +332,6 @@ app.get("/getAllProducts", (req, res) => {
   });
 });
 
-app.post("/sendOrder", (req, res) => {
-  console.log("\n[API endpoint - send Order]\n\nOrder to add: ");
-  console.log(req.body);
-
-  res.set({ "Access-Control-Allow-Origin": "*" });
-
-  writeOrderInDB(req.body).then((response) => {
-    console.log("\nResponse is:");
-    console.log(response);
-    console.log();
-    res.send(response);
-  });
-});
 
 app.listen(3001, () => console.log('Listening on port 3001...'));
 
