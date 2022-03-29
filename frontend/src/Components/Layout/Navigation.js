681eb93aad188f56839e3d0e015937cc937eb3af
KellyA <kelly.acoca@gmail.com>
2022-03-29 18:16:12 -0400

    I just rearanged the navigation bar

--- frontend/src/Components/Layout/Navigation.js
+++ frontend/src/Components/Layout/Navigation.js
@@ -18,9 +18,9 @@ function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/cartpage">Cart ({items.length})</Nav.Link>
         </Nav>
         <Nav>
+          {userState && <Nav.Link  to="/cartpage">Cart ({items.length})</Nav.Link>}
           {userState && <Nav.Link as={Link} to="/myorders">My Orders</Nav.Link> }
           {userState &&  <Nav.Link as={Link} to="/editprofile">Manage My Profile</Nav.Link> }
           {!userState &&  <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link> }
