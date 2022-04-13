e513afd0fa599c62dad812446645af9d065d9241
Nolan Bastien <80637967+nolanbastien@users.noreply.github.com>
2022-04-13 10:21:38 -0400

    Fix: Added fields to sign up form (#131)
    * Add fields to sign up form
    
    * Undo Mistake
    
    * Create node.js.yml
    
    * Revert "Create node.js.yml"
    
    This reverts commit 9d7557a2897e68fcb116514aee5db07507ad1794.
    
    * Revert "Undo Mistake"
    
    This reverts commit a689627fde4c15641c0c1a3050de66b13220d132.
    
    Co-authored-by: emrb9 <93956512+emrb9@users.noreply.github.com>
    Co-authored-by: cnfrancis <37379852+cnfrancis@users.noreply.github.com>

--- frontend/src/Components/Pages/SignUpPage.js
+++ frontend/src/Components/Pages/SignUpPage.js
@@ -9,6 +9,9 @@ function SignUpPage() {
     const [full_name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [gender, setGender] = useState("Male");
+    const [address, setAddress] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
     const [accountType, setAccountType] = useState("Customer");
 
     const handleSubmit = async (event) => {
@@ -24,6 +27,9 @@ function SignUpPage() {
                 email: email,
                 password: password,
                 account_type: accountType,
+                gender: gender,
+                home_address: address,
+                phone_number: phoneNumber
                 }),
             });
             let resJson = await res.json();
@@ -48,7 +54,7 @@ function SignUpPage() {
         <Container fluid style={{ display: 'flex', justifyContent: 'center' }}>
             
             <h1 onClick={() => {navigate('/')}} style={{marginTop: '4%', fontWeight:"bold"}}>BOREAL.ca</h1> 
-            <Card style={{ width: '18rem', position: 'absolute', top: '15%', outline: "solid" }}>
+            <Card style={{ width: '30rem', position: 'absolute', top: '15%', outline: "solid" }}>
             
                 <Card.Body>
                     <Card.Title style={{ textAlign: "center" }}>Create Account</Card.Title>
@@ -66,6 +72,24 @@ function SignUpPage() {
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Create Your Password</Form.Label>
                             <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Please Enter a password" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Label>Gender</Form.Label>
+                            <Form.Select value={accountType} onChange={(e) => setGender(e.target.value)}>
+                                <option>Male</option>
+                                <option>Female</option>
+                            </Form.Select>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Label>Home Address</Form.Label>
+                            <Form.Control type="address" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Please Enter home address" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control type="phoneNumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder="Please Enter phone number" />
                         </Form.Group>
 
                         <Form.Group className="mb-3">
